7b3c02e6d16abf8a8da09c8a339c11ecd8f55ea5
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2018-12-16 17:40:41 +0900

    Set up the common variable before test

--- app/src/test/java/io/github/ykws/moneyexample/InputCheckerTest.kt
+++ app/src/test/java/io/github/ykws/moneyexample/InputCheckerTest.kt
@@ -1,6 +1,5 @@
 package io.github.ykws.moneyexample
 
-import android.renderscript.ScriptGroup
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.After
 import org.junit.Before
@@ -9,9 +8,11 @@ import org.junit.Assert.*
 import org.junit.Test
 
 class InputCheckerTest {
+  lateinit var target: InputChecker
 
   @Before
   fun setUp() {
+    target = InputChecker()
   }
 
   @After
@@ -20,21 +21,18 @@ class InputCheckerTest {
 
   @Test
   fun isValid() {
-    val target = InputChecker()
     val actual = target.isValid("foo")
     assertThat(actual, `is`(true))
   }
 
   @Test
   fun isValid_givenLessThan3_returnsFalse() {
-    val target = InputChecker()
     val actual = target.isValid("ab")
     assertThat(actual, `is`(false))
   }
 
   @Test
   fun isValid_givenAlphaNumeric_returensTrue() {
-    val target = InputChecker()
     val actual = target.isValid("abc123")
     assertThat(actual, `is`(true))
   }
