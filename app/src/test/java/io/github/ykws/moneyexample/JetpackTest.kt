40cdf4cb409a63855ce6be417b3d85d67b7673f0
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2018-12-24 16:02:48 +0900

    Local Unit Test with Jetpack

---
+++ app/src/test/java/io/github/ykws/moneyexample/JetpackTest.kt
@@ -0,0 +1,19 @@
+package io.github.ykws.moneyexample
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Test
+
+import org.junit.Assert.*
+
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class JetpackTest {
+  @Test
+  fun gettingContextTest() {
+    val context = InstrumentationRegistry.getTargetContext()
+    val appName = context.getString(R.string.app_name)
+    assertEquals(appName, "MoneyExample")
+  }
+}
