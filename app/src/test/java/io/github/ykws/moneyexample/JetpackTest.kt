e94d465e77e5862cac84bbaf5f31dbef60da52c7
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2018-12-29 06:03:22 +0900

    Use android.test.platform.app.InstrumentationRegistry instead
    fixes #2

--- app/src/test/java/io/github/ykws/moneyexample/JetpackTest.kt
+++ app/src/test/java/io/github/ykws/moneyexample/JetpackTest.kt
@@ -1,6 +1,6 @@
 package io.github.ykws.moneyexample
 
-import androidx.test.InstrumentationRegistry
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith
 class JetpackTest {
   @Test
   fun gettingContextTest() {
-    val context = InstrumentationRegistry.getTargetContext()
+    val context = InstrumentationRegistry.getInstrumentation().targetContext
     val appName = context.getString(R.string.app_name)
     assertEquals(appName, "MoneyExample")
   }
